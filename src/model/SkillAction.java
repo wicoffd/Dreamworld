5bfc0bb33812825b85a89df3c02090c9c9c7e04a
Derek Wicoff <98190530+wicoffd@users.noreply.github.com>
2023-05-06 18:28:13 -0700

    BigInteger Research
    BigInteger doesn't seem viable for a scalable multiplayer online game. This would be fine for a single player game however.

--- src/model/SkillAction.java
+++ src/model/SkillAction.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 
 public class SkillAction {
@@ -8,6 +9,7 @@ public class SkillAction {
     private final int XP_INDEX = 2;
     private final int SKILL_COUNT = 136;
     private int totalLevel = 0;
+
     static ArrayList<Object>[] skills = new ArrayList[3];
     static String[] skillNames = new String[5];
     ArrayList<Object> xpNeeded = new ArrayList<>(MAX_LEVEL);
@@ -28,12 +30,23 @@ public class SkillAction {
         switch(skill.toString().toLowerCase()){
             case "blocking":
                 skills[2].set(XP_INDEX, xp);
+                getNewLevel(skill, skills[2].get(XP_INDEX));
                 break;
 
 
         }
     }
 
+    private int getNewLevel(Object skill, Object currentXP){
+        int xp = (Integer) currentXP;
+        switch(skill.toString().toLowerCase()){
+            case ("blocking"):
+
+                break;
+
+        }
+        return 1;
+    }
     private void getLevelCalculator(Object skill){
 
     }
@@ -63,16 +76,19 @@ public class SkillAction {
 
         SkillAction skillAction = new SkillAction();
 
+        BigInteger big = new BigInteger("123456789123456789"); // BigInteger is an immutable class. So whenever you do any arithmetic, you have to reassign the output to a variable.
+        // TODO figure out a way to overcome this roadblock. BigInteger values are not scalable to a multiplayer game due to the nature of adding and assigning new values.
 
+        //skillAction.setLevel( "blocking", 2 );
+        skillAction.setXP("blocking", 200);
 
-        skillAction.setLevel( "blocking", 2 );
-        skillAction.setXP("blocking", 500);
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3 ; j++) {
                 System.out.print(skillAction.skills[i].get(j) + " ");
             }
             System.out.println();
         }
+
     }
 
 }
